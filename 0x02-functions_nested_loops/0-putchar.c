cead3d1e6097d61de71f3db483ebe1c1ae96bae4
Abdulhafiz Muhammed <muhammedabdulhafiz1993@gmail.com>
2023-07-19 06:28:13 -0700

    Adding a Directory 0x02-functions_nested_loops to repo

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return 0;
+}
